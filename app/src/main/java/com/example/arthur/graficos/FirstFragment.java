99cf5afd740ce80cb5fd1500f22cd5e8f63a9f73
ArthurLuckwu <arthurvluckwu@gmail.com>
2015-05-03 15:43:50 -0300

    Adicionado JSON no gráfico de barra

--- app/src/main/java/com/example/arthur/graficos/FirstFragment.java
+++ app/src/main/java/com/example/arthur/graficos/FirstFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,48 +16,75 @@ import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class FirstFragment extends Fragment {
 
-        public static FirstFragment newInstance() {
-            FirstFragment fragment = new FirstFragment();
-            return fragment;
-        }
+    public static FirstFragment newInstance() {
+        FirstFragment fragment = new FirstFragment();
+        return fragment;
+    }
 
-        public FirstFragment() {
-        }
+    public FirstFragment() {
+    }
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.grafico1, null);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.grafico1, null);
 //            this.startActivity(new Intent(this.getActivity(), Grafico1.class));
 
-            Log.i(null, "AQUIIII");
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        String response = makeRequest("http://192.168.0.101:3000/teste/all");
+
+        try{
+            JSONArray json = new JSONArray(response);
+            ArrayList<String> bebida = new ArrayList<String>();
+            ArrayList<Integer> consumo = new ArrayList<Integer>();
+
+            for(int i=0;i<json.length();i++){
+                Log.i(null, json.getJSONObject(i).getString("bebida"));
+                bebida.add(json.getJSONObject(i).getString("bebida"));
+                consumo.add(json.getJSONObject(i).getInt("consumo"));
+            }
 
             BarChart chart = (BarChart) rootView.findViewById(R.id.chart);
 
-            Log.i(null, "CHEGOU");
-
-            BarData data = new BarData(getXAxisValues(), getDataSet());
+            BarData data = new BarData(bebida, getDataSet(consumo));
             chart.setData(data);
             chart.setDescription("My Chart");
             chart.animateXY(2000, 2000);
             chart.invalidate();
 
-
-
-            return rootView;
+        }catch (JSONException e) {
+            e.printStackTrace ();
+            Log.i(null, "DEU ERRO");
         }
 
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(2);
-        }
+        return rootView;
+    }
 
-    private ArrayList<BarDataSet> getDataSet() {
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(2);
+    }
+
+
+    private ArrayList<BarDataSet> getDataSet(ArrayList<Integer> consumo) {
+
         ArrayList<BarDataSet> dataSets = null;
+        ArrayList<BarDataSet> dataSet = null;
 
         ArrayList<BarEntry> valueSet1 = new ArrayList<>();
         BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
@@ -91,11 +119,27 @@ public class FirstFragment extends Fragment {
         BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
         barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
 
+
+        ArrayList<BarEntry> valueSet = new ArrayList<>();
+        for(int i=0;i< consumo.size() ;i++){
+            BarEntry b = new BarEntry(consumo.get(i), i);
+            valueSet.add(b);
+        }
+
+        BarDataSet dados = new BarDataSet(valueSet,"Consumo");
+        dados.setColors(ColorTemplate.COLORFUL_COLORS);
+
+
         dataSets = new ArrayList<>();
         dataSets.add(barDataSet1);
         dataSets.add(barDataSet2);
-        return dataSets;
+
+        dataSet = new ArrayList<>();
+        dataSet.add(dados);
+
+        return dataSet;
     }
+
 
     private ArrayList<String> getXAxisValues() {
         ArrayList<String> xAxis = new ArrayList<>();
@@ -107,4 +151,45 @@ public class FirstFragment extends Fragment {
         xAxis.add("JUN");
         return xAxis;
     }
+
+    private String makeRequest(String urlAddress){
+        HttpURLConnection con = null;
+        URL url = null;
+        String response = null;
+        try{
+            url = new URL(urlAddress);
+            con = (HttpURLConnection) url.openConnection();
+            response = readStream(con.getInputStream());
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally {
+            con.disconnect();
+        }
+//        Log.i(null, response);
+        return response;
+    }
+
+    private String readStream ( InputStream in ) {
+        BufferedReader reader = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = null;
+            while((line = reader.readLine()) != null){
+                builder.append(line+ "\n");
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally {
+            if(reader != null){
+                try{
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.toString();
+    }
+
 }
